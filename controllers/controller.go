ed573a87c06b7e51a708506ca954f27c7a7f02c9
Gabriel Sene <sene300@gmail.com>
2024-06-07 18:54:34 -0300

    fix: withou return fix

--- controllers/controller.go
+++ controllers/controller.go
@@ -19,10 +19,6 @@ func GetAllBroths(c *gin.Context) {
 	c.JSON(http.StatusAccepted, &broths)
 }
 
-// func CreateOrderId(c *gin.Context) {
-// 	c.JSON(http.StatusCreated, client.CreateReqWithExternalAPI())
-// }
-
 func GetAllProteins(c *gin.Context) {
 	var proteins []models.Protein
 	database.DB.Find(&proteins)
@@ -62,5 +58,6 @@ func CreateOrder(c *gin.Context) {
 	}
 	order := models.Order{Id: idOrder, Description: proteinSearch.Name + " " + brothSearch.Name, Image: "https://tech.redventures.com.br/icons/ramen/ramenChasu.png"}
 	database.DB.Save(&order)
+	c.JSON(http.StatusOK, order)
 
 }
